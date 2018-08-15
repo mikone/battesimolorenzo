1db09e4be48ff3c9f5dd7b5ac334cf1f54896ee9
Michele Criscuolo <crisc.mic@gmail.com>
2018-08-15 23:03:01 +0200

    aggiunte alcune immagini fixata l'immagine principale dell'header e inserito un metodo javascrpt per inviare le mail

--- js/simplyCountdown.js
+++ js/simplyCountdown.js
@@ -98,10 +98,17 @@
             return {
                 days: createCountdownElt(countdown, parameters, 'simply-days-section'),
                 hours: createCountdownElt(countdown, parameters, 'simply-hours-section'),
-                minutes: createCountdownElt(countdown, parameters, 'simply-minutes-section'),
-                seconds: createCountdownElt(countdown, parameters, 'simply-seconds-section')
+                minutes: createCountdownElt(countdown, parameters, 'simply-minutes-section')
             };
         }
+        // if (!parameters.inline) {
+        //     return {
+        //         days: createCountdownElt(countdown, parameters, 'simply-days-section'),
+        //         hours: createCountdownElt(countdown, parameters, 'simply-hours-section'),
+        //         minutes: createCountdownElt(countdown, parameters, 'simply-minutes-section'),
+        //         seconds: createCountdownElt(countdown, parameters, 'simply-seconds-section')
+        //     };
+        // }
 
         spanTag = document.createElement('span');
         spanTag.classList.add(parameters.inlineClass);
